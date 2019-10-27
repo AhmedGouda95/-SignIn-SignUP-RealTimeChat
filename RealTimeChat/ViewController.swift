c202b0c75c8c357cd88fc319141aa1c43005fa22
Ahmed Gouda <ahmedgouda@Ahmeds-MacBook-Pro.local>
2019-10-27 12:39:48 -0700

    Initial Commit

---
+++ RealTimeChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealTimeChat
+//
+//  Created by Ahmed Gouda on 10/27/19.
+//  Copyright © 2019 Ahmed Gouda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
